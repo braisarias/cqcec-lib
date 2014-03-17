8ea64be7b2714f40b35f67df98ebbf7aa25e5af5
Marcos Chavarría Teijeiro <chavarria1991@gmail.com>
2014-03-17 12:39:13 +0100

    Implement setup.py script.

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# "Con quien carallo estoy conectado"
+# Copyright (C) 2014  Marcos Chavarría Teijeiro.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+dependency_links = ["https://github.com/juliensobrier/google-safe" +
+                    "-browsing-lookup-python.git"]
+install_requires = ["requests", "BeautifulSoup", "whois"]
+
+setup(
+    name="CQCEC",
+    version="0.01alpha",
+    scripts=["cqcec"],
+    packages=["cqcec_lib"],
+    install_requires=["requests", "BeautifulSoup", "whois"],
+    dependency_links=[
+        "https://github.com/juliensobrier/google-safe"]
+)
